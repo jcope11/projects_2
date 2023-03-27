6fa549dcf039b52acb1f8cd057034cae051a530b
Joe Copeland <jcope11@gmail.com>
2023-03-26 19:26:54 -0700

    first commit for powerball game

---
+++ venv/power_ball.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+# Powerball simulator
+# 5 white balls chosen from 1 to 69.
+# 1 red ball chosen from 1 to 26.
+
+from random import randint
+
+# white and red balls list creation
+white_balls, red_balls = [], []
+for i in range(1, 70):
+    white_balls.append(i)
+for i in range(1, 27):
+    red_balls.append(i)
+
+print(white_balls)
+print(red_balls)
